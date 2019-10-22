a76bd223c3052cb472f9256cafcbd51fea227a0e
Kiyotaka <t10kiyotaka@gmail.com>
2019-10-22 22:18:49 +0900

    Add onSubmit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { Comment } from './class/chat';
+import { Comment, User } from './class/chat';
 
+
+const CURRENT_USER: User = new User(1, 'Tanaka Taro');
+const ANOTHER_USER: User = new User(2, 'Suzuki Jiro');
 
 const COMMENTS: Comment[] = [
-  { name: 'Suzuki Taro', content: 'This is comment 1.' },
-  { name: 'Suzuki Taro', content: 'This is comment 2.' },
-  { name: 'Suzuki Taro', content: 'This is comment 3.' },
+  new Comment(ANOTHER_USER, 'This is the first comment by Suzuki.'),
+  new Comment(ANOTHER_USER, 'This is the second comment by Suzuki.'),
+  new Comment(CURRENT_USER, 'This is the first comment by Tanaka.'),
+  new Comment(ANOTHER_USER, 'This is the third comment by Suzuki.'),
+  new Comment(CURRENT_USER, 'This is the first comment by Tanaka.'),
 ]
 
 @Component({
@@ -14,7 +19,19 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public content = '';
-  public comments = COMMENTS;
+  content = '';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
 
+
+  isCurrentUser(targetUid: number): boolean {
+    return this.currentUser.uid === targetUid;
+  }
+
+  addComment(comment: string) {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+      this.content = '';
+    }
+  }
 }
--- src/app/class/chat.ts
+++ src/app/class/chat.ts
@@ -1,4 +1,23 @@
-export class Comment {
+export class User {
+  uid: number;
   name: string;
+
+  constructor(uid: number, name: string) {
+    this.uid = uid;
+    this.name = name;
+  }
+}
+
+
+export class Comment {
+  user: User;
+  initial: string;
   content: string;
+
+  constructor(user: User, content: string) {
+    this.user = user;
+    this.initial = user.name.slice(0, 1);
+    this.content = content;
+  }
+
 }
